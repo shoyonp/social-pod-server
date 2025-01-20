4546bc99bba24d44976433bffcd8242c2ee1bf9a
shoyonp <shoyonkumar143@gmail.com>
2025-01-21 03:16:38 +0600

    users posted data gets from databse

--- index.js
+++ index.js
@@ -40,6 +40,15 @@ async function run() {
       res.send(result);
     });
 
+    // get the posts data user added
+    app.get("/myPost/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { authorEmail: email };
+      const result = await postCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    // get specific post data 
     app.get(`/post/:id`, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
